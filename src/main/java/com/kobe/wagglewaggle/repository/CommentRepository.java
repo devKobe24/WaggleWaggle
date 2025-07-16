9ed97b1b0dbc197d3076c2bbc36cb255f1453d98
Kobe <dev.skyachieve91@gmail.com>
2025-07-16 14:08:18 +0900

    delete: findByPostIdOrderByCreatedAtAsc
    - findAllWithPostByPostId(@Param("postId") Long postId) 쿼리 메서드 새롭게 생성 및 구현.

--- src/main/java/com/kobe/wagglewaggle/repository/CommentRepository.java
+++ src/main/java/com/kobe/wagglewaggle/repository/CommentRepository.java
@@ -2,9 +2,12 @@ package com.kobe.wagglewaggle.repository;
 
 import com.kobe.wagglewaggle.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	List<Comment> findByPostIdOrderByCreatedAtAsc(Long postId);
+	@Query("SELECT c FROM Comment c JOIN FETCH c.post WHERE c.post.id = :postId ORDER BY c.createdAt ASC")
+	List<Comment> findAllWithPostByPostId(@Param("postId") Long postId);
 }
