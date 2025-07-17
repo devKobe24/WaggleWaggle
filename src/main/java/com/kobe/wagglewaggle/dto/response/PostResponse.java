9dc8800f3947eb2d0bf0c041dfbc8c710da8435c
Kobe <dev.skyachieve91@gmail.com>
2025-07-17 10:32:37 +0900

    fix: Jackson 직렬화를 위한 코드 수정 및 구현.

--- src/main/java/com/kobe/wagglewaggle/dto/response/PostResponse.java
+++ src/main/java/com/kobe/wagglewaggle/dto/response/PostResponse.java
@@ -1,26 +1,34 @@
 package com.kobe.wagglewaggle.dto.response;
 
 import com.kobe.wagglewaggle.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostResponse {
 
-	private final Long id;
-	private final String title;
-	private final String content;
-	private final String author;
-	private final LocalDateTime createdAt;
-	private final LocalDateTime updatedAt;
+	private Long id;
+	private String title;
+	private String content;
+	private String author;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 
-	public PostResponse(Post post) {
-		this.id = post.getId();
-		this.title = post.getTitle();
-		this.content = post.getContent();
-		this.author = post.getAuthor();
-		this.createdAt = post.getCreatedAt();
-		this.updatedAt = post.getUpdatedAt();
+	public static PostResponse from(Post post) {
+		return PostResponse.builder()
+			.id(post.getId())
+			.title(post.getTitle())
+			.content(post.getContent())
+			.author(post.getAuthor())
+			.createdAt(post.getCreatedAt())
+			.updatedAt(post.getUpdatedAt())
+			.build();
 	}
 }
